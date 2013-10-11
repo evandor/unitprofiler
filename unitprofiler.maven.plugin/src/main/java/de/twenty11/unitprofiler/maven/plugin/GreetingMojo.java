f13dd5f9c59e7c3d8bc1e83906a8c9e6fbe82188
Carsten Gräf <evandor@gmail.com>
2013-10-11 16:31:20 +0200

    wip

--- unitprofiler.maven.plugin/src/main/java/de/twenty11/unitprofiler/maven/plugin/GreetingMojo.java
+++ unitprofiler.maven.plugin/src/main/java/de/twenty11/unitprofiler/maven/plugin/GreetingMojo.java
@@ -2,7 +2,9 @@ package de.twenty11.unitprofiler.maven.plugin;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
 
+@Mojo(name = "sayhi")
 public class GreetingMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException {
         getLog().info("Hello, world.");
